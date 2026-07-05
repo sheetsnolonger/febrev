c793b8e9bea8a731fc241ce41dc068e98d199c3b
sheetsnolonger <sam.personalt@gmail.com>
2026-07-05 15:50:40 -0400

    Update server.js

--- server.js
+++ server.js
@@ -8,26 +8,31 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-const products = [
-{
-  id: 1,
-  name: "medium black handmade stuffed crochet kitty",
-  price: 20,
-  image: "",
-  description: "this crocheted kitty is not only a stuffed animal, but a friend. crocheted with black yarn, white button eyes, and a white X mouth.",
-  ebay: "https://www.ebay.com/"
-}
-];
+const febProducts = [
+  {
+    id: 1,
+    name: "medium black handmade stuffed crochet kitty",
+    price: 20,
+    image: "",
+    description: "this crocheted kitty is not only a stuffed animal, but a friend. crocheted with black yarn, white button eyes, and a white X mouth. ",
+    ebay: "https://www.ebay.com/"
+  },
+
+const revProducts = [
+
+
+const allProducts = [...febProducts, ...revProducts];
 
 app.get("/", (req, res) => {
   res.render("index", {
     title: "feb & rev",
-    products
+    febProducts,
+    revProducts
   });
 });
 
 app.get("/product/:id", (req, res) => {
-  const product = products.find(p => p.id === Number(req.params.id));
+  const product = allProducts.find(p => p.id === Number(req.params.id));
 
   if (!product) {
     return res.status(404).send("product not found");
